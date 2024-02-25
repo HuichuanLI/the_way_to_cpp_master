73d7d3bd3c31446a8708d508b5e3892945126ff0
lihuichuan <lhc14124908@163.com>
2024-02-25 17:27:04 +0800

    add

--- 对象优化02/对象优化02.cpp
+++ 对象优化02/对象优化02.cpp
@@ -1,18 +1,22 @@
 ﻿#include <iostream>
+
 using namespace std;
 
-class Test
-{
+class Test {
 public:
     Test(int data = 10) : ma(data) { cout << "Test(" << ma << ")" << endl; }
+
     ~Test() { cout << "~Test(" << ma << ")" << endl; }
-    Test(const Test& t) : ma(t.ma) { cout << "Test(const Test& t)" << endl; }
-    void operator = (const Test& t) 
-    {
+
+    Test(const Test &t) : ma(t.ma) { cout << "Test(const Test& t)" << endl; }
+
+    void operator=(const Test &t) {
         cout << "operator =" << endl;
         ma = t.ma;
     }
+
     int getData() const { return ma; }
+
 private:
     int ma;
 };
@@ -28,8 +32,7 @@ Test GetObject(Test &t) // 不能返回局部的或者临时的对象的指针�
     // 只能通过临时对象传出去，但是vs22并未调用拷贝构造
 }
 
-int main()
-{
+int main() {
     Test t1;
     //Test t2;
     cout << "-------------" << endl;
